e94a14fe63431b5fb0f51d41c36702b13fffa231
juviyu <juvijuviyu@gmail.com>
2023-11-12 08:57:10 +0000

    compiling on humble

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,6 @@
-docker run -d --network=host -v `pwd`/mavros:/user/mavros_test/mavros mavros_test_sitl
-docker run -d --network=host -v `pwd`/mavros:/user/mavros_test/mavros mavros_test_mavros_node
+#!/bin/bash
+
+read -p "Make sitl is running (press enter) "
+
+# Run the MAVROS node
+ros2 run mavros mavros_node --ros-args --params-file mavros_config.yaml
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-vcs import < src/ros2.repos src
+# vcs import src < src/ros2.repos
+pip install future
+
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
